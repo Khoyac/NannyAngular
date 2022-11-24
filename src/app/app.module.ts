6c18cdc5bb27bba259b470cb467543b83c9cb476
Khoyac <khoyac@gmail.com>
2022-11-24 18:50:32 +0100

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TomNavBarComponent } from './tom-nav-bar/tom-nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { PortadaComponent } from './portada/portada.component';
+import { CircleComponent } from './circle/circle.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TomNavBarComponent,
+    PortadaComponent,
+    CircleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tom-nav-bar/tom-nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tom-nav-bar',
+  templateUrl: './tom-nav-bar.component.html',
+  styleUrls: ['./tom-nav-bar.component.css']
+})
+export class TomNavBarComponent {
+
+}
